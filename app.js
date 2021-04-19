f2901563a7fad8ccdcd4dacb0821117b6b0751f7
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-19 17:49:18 +0900

    [Update]mouseover,mouseout

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 $(function(){
-  $('.box1').slideDown(function(){
-    $('.box1').css({
-      'background-color':'#0000FF',
-      'width':'200px',
-      'height':'100px'
-    }).slideUp();
+  $('.box1').mouseover(function(){
+    $('.box1').css({'background-color':'#0000FF'});
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').css({'background-color':'#FF0000'});
   });
 });
 
