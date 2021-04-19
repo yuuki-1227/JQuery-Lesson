a6676d82a70b1ea427f9394b15f42e91085dd01a
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-19 17:41:03 +0900

    [Update]JQuery記載

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 $(function(){
-  $('.box1').slideUp();
+  $('.box1').slideDown(function(){
+    $('.box1').css({
+      'background-color':'#0000FF',
+      'width':'200px',
+      'height':'100px'
+    }).slideUp();
+  });
 });
+
