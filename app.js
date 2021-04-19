618d0b5a86670dd4afebf107a0dd0d25f0dccff6
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-19 18:10:49 +0900

    [Update]this

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
 $(function(){
-  $('.bg1').on('click',function(){
-    $('.bg1').slideUp();
+  $('.box1').on('click',function(){
+    $(this).slideUp();
   });
-
-    $('.bg2').on('click',function(){
-      $('.bg2').slideUp();
-    });
-
-  
-    $('.bg3').on('click',function(){
-      $('.bg3').slideUp();
-    });
-  
-    $('.bg4').on('click',function(){
-      $('.bg4').slideUp();
-    });
-  });
+});
