2f8264df2c7c65257844a2d749e64a418f59a710
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-19 17:55:13 +0900

    [Update]イベント追加

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 $(function(){
-  $('.box1').mouseover(function(){
-    $('.box1').css({'background-color':'#0000FF'});
+  $('.box1').on('click',function(){
+    $('.box1').addClass('box1-ext');
   });
   $('.box1').mouseout(function(){
-    $('.box1').css({'background-color':'#FF0000'});
+    $('.box1').removeClass('box1-ext');
   });
 });
 
